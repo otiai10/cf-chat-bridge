2e21f6b827dff3d40cb79b42d7ae1f8dca8f9804
Hiromu OCHIAI <otiai10@gmail.com>
2018-05-10 16:19:31 +0900

    Fix #2

--- functions/index.js
+++
@@ -1,9 +0,0 @@
-const bridge = require("./cloud-chat-bridge");
-
-const vars = require("./secret.json");
-bridge.init(vars);
-
-const rules = require("./rules");
-const endpoint = bridge.hook("LINE").pass(rules).endpoint();
-
-exports.line = endpoint;
