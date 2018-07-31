17c90000f4b4d0b95014c9eb07ddf9d45da24521
Hiromu OCHIAI <otiai10@gmail.com>
2018-07-31 12:51:03 +0900

    Add Travis-CI

--- tests/data/example-vars.js
+++ tests/data/example-vars.js
@@ -1,4 +1,9 @@
-const secrets = require("./test-secret-vars");
+var secrets = {};
+try {
+  secrets = require("./test-secret-vars");
+} catch (e) {
+  // do nothing.
+}
 
 module.exports = {
   "GOOGLE_API_KEY": process.env.GOOGLE_API_KEY || secrets.GOOGLE_API_KEY
