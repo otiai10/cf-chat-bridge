2756919af3268f40d54be807689e1e5f8b36d411
Hiromu OCHIAI <otiai10@gmail.com>
2019-07-05 13:48:28 +0900

    Fix testing style

--- tests/story/transforms-test.ts
+++ tests/story/transforms-test.ts
@@ -7,43 +7,37 @@ const vars = require("../data/example-vars");
 const messages = require("../data/messages.json");
 
 describe("createHandler", () => {
-  it("create handlers with default transforms", (ok) => {
+  it("create handlers with default transforms", async () => {
     const rules = require("../data/example-rules");
     const h = createHandler(rules[0], vars)[0] as LineHandler;
     expect((h as any).transforms.length).toBe(1);
     expect((h as any).transforms[0]).toBeInstanceOf(LineToSlack);
 
     const exp = new Exposer(h);
-    exp.transform(messages.case_00).then(entry => {
-      expect(entry.transformed.text).toBe("こんにちは");
-      ok();
-    });
+    const entry = await exp.transform(messages.case_00);
+    expect(entry.transformed.text).toBe("こんにちは");
   });
 });
 
 describe("When custom transforms are given", () => {
-  it("transforms payload in series", (ok) => {
+  it("transforms payload in series", async () => {
     const rule = require("../data/example-rules")[1];
     const h = createHandler(rule, vars)[0] as LineHandler;
     expect((h as any).transforms.length).toBe(2);
     expect((h as any).transforms[0]).toBeInstanceOf(LineToSlack);
     const exp = new Exposer(h);
-    exp.transform(messages.case_00).then(entry => {
-      expect(entry.transformed.text).toBe("こんにちは!!!!!!!!!!!!!!!!!!!!");
-      ok();
-    });
+    const entry = await exp.transform(messages.case_00);
+    expect(entry.transformed.text).toBe("こんにちは!!!!!!!!!!!!!!!!!!!!");
   });
 });
 
 describe("When google translate transform is given", () => {
-  it("should translate the text", (ok) => {
+  it("should translate the text", async () => {
     const rule = require("../data/example-rules")[2];
     const h = createHandler(rule, vars)[0] as LineHandler;
     const exp = new Exposer(h);
-    exp.transform(messages.case_00).then(entry => {
-      expect(entry.transformed.text).toBe("Hello");
-      ok();
-    });
+    const entry = await exp.transform(messages.case_00);
+    expect(entry.transformed.text).toBe("Hello");
   });
 });
 
