0c8e02198e66c719474190ef787594a7068a0e30
Hiromu OCHIAI <otiai10@gmail.com>
2021-05-25 19:26:40 +0900

    Add Logging

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import BuiltinHandler from "./handler/Builtin";
 import Rule from "./types/Rule";
 import Secrets from "./types/Secrets";
 import Verifier from "./verifier";
+import { Log, Logging } from "@google-cloud/logging";
 
 export interface BridgeConfig {
   secrets: Secrets;
@@ -16,11 +17,13 @@ export class Bridge {
   private secrets: Secrets;
   private verifier: Verifier;
   private handlers: Handler[] = [];
+  private lg: Log;
 
   constructor({ secrets, rules }: BridgeConfig) {
     this.secrets = secrets;
     this.rules = rules;
     this.verifier = new Verifier(secrets);
+    this.lg = (new Logging()).log('cf-chat-bridge');
   }
 
   public endpoint(): (req: express.Request, res: express.Response) => any {
@@ -37,37 +40,40 @@ export class Bridge {
     ];
   }
 
-  private async dispatch(req, res) {
+  private async dispatch(req: express.Request, res: express.Response) {
     if (!this.verifier.verify(req)) {
       res.status(400).end();
       return;
     }
     try {
       const results = await Promise.all(this.handlers.filter(h => h.match(req)).map(h => {
-        console.log(JSON.stringify({
-          severity: "DEBUG",
+        this.log({
           message: `MATCH: ${(h as any).constructor.name}`,
           handler: (h as any).constructor.name,
-        }));
+          body: req.body,
+        }, { component: 'bridge' }, 'DEBUG');
         return h.handle(req);
       }));
       const result = results.length === 1 ? results[0] : results;
-      console.log(JSON.stringify({
-        severity: "DEBUG",
+      this.log({
         message: `RESULT`,
         result,
         query: req.query,
         body: req.body,
-      }));
+      }, { component: 'bridge' }, 'DEBUG');
       res.status(200).json(results);
     } catch (err) {
-      console.error(JSON.stringify({
-        severity: "ERROR",
+      this.log({
         message: err.message,
         stack: err.stack,
         code: err.code,
-      }));
+      }, { component: 'bridge' }, 'ERROR');
       res.status(500).json(err);
     }
+  }
+
+  private async log(payload: any, labels: { [key: string]: string } = {}, severity = 'INFO'): Promise<any> {
+    const entry = this.lg.entry({ labels, severity }, payload);
+    return await this.lg.write(entry);
   }
 }
